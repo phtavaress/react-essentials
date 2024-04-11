a034f501feb7dc15c59709e4c60d8983255a4b63
Pedro Tavares <ph_tavares@hotmail.com>
2024-04-11 11:24:16 -0300

    Projeto criado

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Conceitos fundamentais do React que vão te ajudar em quase todos os apps
+          que você for contruir!
+        </p>
+      </header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
