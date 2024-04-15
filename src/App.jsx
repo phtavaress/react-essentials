2ff617487286c1e8cbfc75b87dd6ceeb787417d6
Pedro Tavares <ph_tavares@hotmail.com>
2024-04-15 09:55:55 -0300

    Exibindo adjetivos de maneira aleatória

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+const reactDescriptions = ['fundamentais', 'cruciais', 'principais'];
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+const Header = () => {
+
+  const description = reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
+
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Conceitos {description} do React que vão te ajudar em quase todos os apps
+        que você for construir!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Conceitos fundamentais do React que vão te ajudar em quase todos os apps
-          que você for contruir!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <h2>Vamos lá!</h2>
       </main>
     </div>
   );
